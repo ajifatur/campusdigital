49767b8ff4e9bd621a94349ad1e99b128d67b58d
ajifatur <ajifatur2@gmail.com>
2021-03-15 15:25:20 +0700

    update sertifikat

--- resources/views/e-sertifikat/pdf/peserta/all.blade.php
+++ resources/views/e-sertifikat/pdf/peserta/all.blade.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<!--<link rel="stylesheet" type="text/css" href="https://fonts.googleapis.com/css?family=Lato:400,700" />-->
+	<!--<link rel="stylesheet" type="text/css" href="https://fonts.googleapis.com/css?family=Lato:400,700" /> #fff57e-->
 	<title>Sertifikat Peserta Pelatihan</title>
 	<link rel="shortcut icon" href="{{ asset('assets/images/logo/'.get_icon()) }}">
 	<style type="text/css">
@@ -12,9 +12,10 @@
 		@page {margin: 0px;}
 		html {margin: 0px;}
 		body {margin: 0px; font-family: 'Lato-Regular'; font-size: 18.5px; background-color: {{ get_warna_background_sertifikat() }};}
+		.pattern {position: absolute; top: 0px; left: 0px; width: 100%;}
 		#nomor-seri {font-size: 12px; position: absolute; top: 5px; left: 15px;}
-		#logo {position: absolute; top: 50px; left: 88px; width: 950px; text-align: center;}
-		#img-logo {max-height: 110px;}
+		#logo {position: absolute; left: 88px; top: {{ $pelatihan->id_pelatihan < 18 ? '50px' : '40px' }}; width: 950px; text-align: center;}
+		#img-logo {max-height: {{ $pelatihan->id_pelatihan < 18 ? '110px' : '120px' }};}
 		#text-sertifikat {font-family: 'Lato-Bold'; font-size: 66px; position: absolute; top: 145px; left: 88px; width: 950px; color: #d4b064; text-align: center; text-transform: uppercase;}
 		#nomor-sertifikat {font-size: 29px; position: absolute; top: 242px; left: 88px; width: 950px; text-align: center;}
 		#text-diberikan-kepada {font-family: 'Lato-Bold'; font-size: 23px; position: absolute; top: 302px; left: 88px; width: 950px; text-align: center;}
@@ -66,8 +67,9 @@
 		$status_pelatihan = '';
 @endphp
 <body>
+	<div class="pattern"><img src="{{ asset('assets/images/others/LINE 3-min.png') }}" width="100%"></div>
 	<div id="nomor-seri">Nomor Seri: {{ $member->kode_sertifikat }}</div>
-	<div id="logo"><img id="img-logo" src="{{ asset('assets/images/logo/'.get_logo()) }}"></div>
+	<div id="logo"><img id="img-logo" src="{{ $pelatihan->id_pelatihan < 18 ? asset('assets/images/logo/'.get_logo()) : asset('assets/images/logo/6.png') }}"></div>
 	<div id="text-sertifikat">Sertifikat</div>
 	<div id="nomor-sertifikat">Nomor: {{ $pelatihan->nomor_pelatihan }}</div>
 	<div id="text-diberikan-kepada">Diberikan Kepada:</div>
@@ -97,6 +99,7 @@
 	<div id="line-bg-2"></div>
 	<div id="line-bg-1"></div>
 	<div id="page-break"></div>
+	<div class="pattern"><img src="{{ asset('assets/images/others/LINE 3-min.png') }}" width="100%"></div>
 	<div id="nomor-seri">Nomor Seri: {{ $member->kode_sertifikat }}</div>
 	<div id="text-materi">Materi {{ $pelatihan->kategori }}</div>
 	<div id="table">
