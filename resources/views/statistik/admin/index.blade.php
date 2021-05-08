13c2f3a91da352dde289afa8ed1c31bb3543766d
isna prasetyo <praseetyaa@gmail.com>
2021-05-08 11:32:10 +0700

    add statistic

--- resources/views/statistik/admin/index.blade.php
+++ resources/views/statistik/admin/index.blade.php
@@ -2,13 +2,7 @@
 
 @section('content')
 
-<!-- ============================================================== -->
-<!-- Page wrapper  -->
-<!-- ============================================================== -->
 <div class="page-wrapper">
-    <!-- ============================================================== -->
-    <!-- Bread crumb and right sidebar toggle -->
-    <!-- ============================================================== -->
      <div class="page-breadcrumb">
         <div class="row">
             <div class="col-12 d-flex no-block align-items-center">
@@ -24,44 +18,137 @@
             </div>
         </div>
     </div>
-    <!-- ============================================================== -->
-    <!-- End Bread crumb and right sidebar toggle -->
-    <!-- ============================================================== -->
-    <!-- ============================================================== -->
-    <!-- Container fluid  -->
-    <!-- ============================================================== -->
     <div class="container-fluid">
-        <!-- ============================================================== -->
-        <!-- row -->
-        <!-- ============================================================== -->
         <div class="row">
-            <!-- column -->
-            <div class="col-lg-12">
-                <!-- card -->
+            <div class="col-lg-6">
                 <div class="card shadow">
+                    <div class="card-header bg-transparent">
+                        <p class="font-weight-bold m-0">Usia</p>
+                        <p class="m-0 text-muted">Statistik usia</p>
+                    </div>
+                    <div class="card-body">
+                        <canvas id="cartUsia" width="600" height="400"></canvas>
+                    </div>
                 </div>
-                <!-- card -->
             </div>
-            <!-- column -->
+            <div class="col-lg-6">
+                <div class="card shadow">
+                    <div class="card-header bg-transparent">
+                        <p class="font-weight-bold m-0">Gender</p>
+                        <p class="m-0 text-muted">Statistik Gender</p>
+                    </div>
+                    <div class="card-body">
+                        <canvas id="cartGender" width="600" height="400"></canvas>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-6">
+                <div class="card shadow">
+                    <div class="card-header bg-transparent">
+                        <p class="font-weight-bold m-0">Lokasi</p>
+                        <p class="m-0 text-muted">Statistik Lokasi</p>
+                    </div>
+                    <div class="card-body">
+                        <canvas id="cartLokasi" width="600" height="400"></canvas>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-6">
+                <div class="card shadow">
+                    <div class="card-header bg-transparent">
+                        <p class="font-weight-bold m-0">Kunjungan</p>
+                        <p class="m-0 text-muted">Statistik Kunjungan</p>
+                    </div>
+                    <div class="card-body">
+                        <canvas id="cartKunjungan" width="600" height="400"></canvas>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-6">
+                <div class="card shadow">
+                    <div class="card-header bg-transparent">
+                        <p class="font-weight-bold m-0">Pelatihan</p>
+                        <p class="m-0 text-muted">Statistik Pelatihan</p>
+                    </div>
+                    <div class="card-body">
+                        <canvas id="cartPelatihan" width="600" height="400"></canvas>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-6">
+                <div class="card shadow">
+                    <div class="card-header bg-transparent">
+                        <p class="font-weight-bold m-0">Churn Rate</p>
+                        <p class="m-0 text-muted">Statistik Churn Rate</p>
+                    </div>
+                    <div class="card-body">
+                        <canvas id="cartChurn" width="600" height="400"></canvas>
+                    </div>
+                </div>
+            </div>
         </div>
-        <!-- ============================================================== -->
-        <!-- Recent comment and chats -->
-        <!-- ============================================================== -->
     </div>
-    <!-- ============================================================== -->
-    <!-- End Container fluid  -->
-    <!-- ============================================================== -->
     @include('template/admin/_footer')
 </div>
-<!-- ============================================================== -->
-<!-- End Page wrapper  -->
-<!-- ============================================================== -->
 
 @endsection
 
 @section('js-extra')
-
+<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.5.0/Chart.min.js"></script>
 <script type="text/javascript">
+var canvasUsia = document.getElementById("cartUsia");
+var canvasGender = document.getElementById("cartGender");
+var canvasLokasi = document.getElementById("cartLokasi");
+var canvasKunjungan = document.getElementById("cartKunjungan");
+var canvasPelatihan = document.getElementById("cartPelatihan");
+var canvasChurn = document.getElementById("cartChurn");
+var oilData = {
+    labels: [
+        "Saudi Arabia",
+        "Russia",
+        "Iraq",
+        "United Arab Emirates",
+        "Canada"
+    ],
+    datasets: [
+        {
+            data: [133.3, 86.2, 52.2, 51.2, 50.2],
+            backgroundColor: [
+                "#FF6384",
+                "#63FF84",
+                "#84FF63",
+                "#8463FF",
+                "#6384FF"
+            ]
+        }]
+};
+
+var pieChart = new Chart(cartUsia, {
+  type: 'pie',
+  data: oilData
+});
+var pieChart = new Chart(cartGender, {
+  type: 'pie',
+  data: oilData
+});
+var pieChart = new Chart(cartLokasi, {
+  type: 'pie',
+  data: oilData
+});
+var pieChart = new Chart(cartKunjungan, {
+  type: 'pie',
+  data: oilData
+});
+var pieChart = new Chart(cartPelatihan, {
+  type: 'pie',
+  data: oilData
+});
+var pieChart = new Chart(cartChurn, {
+  type: 'pie',
+  data: oilData
+});
+
+
 </script>
 
 @endsection
